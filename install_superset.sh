77e6a199e15646ad4e09900f2eae6908cb4ac8b2
Ilia Mishin <ilya.mishin@quadcode.com>
2024-01-30 14:21:10 +0300

    add install script

---
+++ install_superset.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Прерывать скрипт при любой ошибке
+set -e
+
+print_data_solutions_banner(){
+echo -e "\033[32m"
+echo -e "____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____"
+echo -e ""
+echo -e " ██████╗  █████╗ ████████╗ █████╗         ███████╗ ██████╗ ██╗     ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗███████╗"
+echo -e " ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗        ██╔════╝██╔═══██╗██║     ██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║██╔════╝"
+echo -e " ██║  ██║███████║   ██║   ███████║        ███████╗██║   ██║██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║███████╗"
+echo -e " ██║  ██║██╔══██║   ██║   ██╔══██║        ╚════██║██║   ██║██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║╚════██║"
+echo -e " ██████╔╝██║  ██║   ██║   ██║  ██║        ███████║╚██████╔╝███████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║███████║"
+echo -e " ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝        ╚══════╝ ╚═════╝ ╚══════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝"
+echo -e ""
+echo -e "____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____"
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e "                                         INSTALLATION FOR DS.BI                                                    "
+echo -e "\033[0m"
+}
+
+print_separator(){
+echo -e "\033[32m"
+echo -e ""
+echo -e "____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____  ____"
+echo -e ""
+echo -e "\033[0m"
+}
+
+# Функция для установки необходимых компонентов: Git и Ansible
+install_requirements() {
+    echo "Установка предварительных пакетов"
+    echo -e ""
+    sudo apt-get update
+    sudo apt-get install -y git ansible
+    echo "Git и Ansible установлены."
+}
+
+# Функция для клонирования репозитория
+clone_repository() {
+    echo -e "Подготовка файлов для установки"
+    if [ -d "$HOME/superset_deploy/.git" ]; then
+        cd "$HOME/superset_deploy"
+        git pull
+    else
+        git clone https://github.com/imishin1/superset_deploy.git "$HOME/superset_deploy"
+    fi
+}
+
+# Функция для перехода в директорию и запуска Ansible playbook
+run_playbook() {
+    echo "Начало установки"
+    cd "$HOME/superset_deploy" && ansible-playbook -i "localhost," -c local ./playbooks_examples/install_with_redis.yml
+    echo "Установка успешно завершена"
+    echo "Superset доступен по порту 8088"
+}
+
+# Основная логика скрипта
+main() {
+    print_data_solutions_banner
+    print_separator
+    install_requirements
+    print_separator
+    clone_repository
+    print_separator
+    run_playbook
+    print_separator
+}
+
+# Запуск основной логики скрипта
+main
